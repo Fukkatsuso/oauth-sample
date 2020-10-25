8b0f77de2c0634ce04df216f2ac1ca13cb38cc0b
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-10-25 19:38:07 +0900

    cookieストアを使ってtwitterのOAuth, プロフィール取得およびツイート投稿取得

--- app/go.mod
+++ app/go.mod
@@ -2,16 +2,30 @@ module github.com/Fukkatsuso/oauth-sample
 
 go 1.14
 
+replace github.com/Fukkatsuso/oauth-sample/app/lib/twitter => ./lib/twitter
+
 require (
+	github.com/Fukkatsuso/oauth-sample/app/lib/twitter v0.0.0-00010101000000-000000000000
+	github.com/boj/redistore v0.0.0-20180917114910-cd5dcc76aeff // indirect
+	github.com/gin-contrib/sessions v0.0.3
+	github.com/gin-gonic/contrib v0.0.0-20201005132743-ca038bbf2944
 	github.com/gin-gonic/gin v1.6.3
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/kidstuff/mongostore v0.0.0-20181113001930-e650cd85ee4b // indirect
+	github.com/kimiazhu/ginweb-contrib v0.0.0-20160408072953-eb842df46c92
+	github.com/kimiazhu/golib v0.0.0-20181123075453-970a1a889a1d // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/mrjones/oauth v0.0.0-20190623134757-126b35219450
 	github.com/ugorji/go v1.1.13 // indirect
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
 	golang.org/x/sys v0.0.0-20201020230747-6e5568b54d1a // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- app/main.go
+++ app/main.go
@@ -1,14 +1,116 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"os"
+
+	"github.com/Fukkatsuso/oauth-sample/app/lib/twitter"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	// mysqlsession "github.com/kimiazhu/ginweb-contrib/sessions"
+)
 
 func main() {
+	endpoint := os.Getenv("MYSQL_USER") + ":" + os.Getenv("MYSQL_PASSWORD") + "@" + os.Getenv("MYSQL_PROTOCOL") + "/" + os.Getenv("MYSQL_DATABASE")
+	endpoint += "?parseTime=true&loc=Local"
+	// store, err := mysqlsession.NewMySQLStore(endpoint, "sessions", []byte("secret"))
+	// if err != nil {
+	// 	panic("failed to create session store")
+	// }
+	store := cookie.NewStore([]byte("secret"))
+	store.Options(sessions.Options{
+		MaxAge: 60 * 60 * 24,
+	})
+
 	r := gin.Default()
+	r.LoadHTMLGlob("views/html/*")
+	// sessions.Sessions(name string, store Store)
+	// type Store interface {
+	// 	sessions.Store: "github.com/gorilla/sessions"
+	// 	Options(Options)
+	// }
+	// sessions.Store interface {
+	// 	Get(r *http.Request, name string) (*Session, error)
+	// 	New(r *http.Request, name string) (*Session, error)
+	// 	Save(r *http.Request, w http.ResponseWriter, s *Session) error
+	// }
+	r.Use(sessions.Sessions("session", store))
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world!",
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "OAuth sample app!",
 		})
+	})
+
+	r.GET("/twitter", func(c *gin.Context) {
+		aToken := twitter.GetAccessToken(c)
+		if aToken == nil {
+			c.Redirect(http.StatusSeeOther, "/twitter/oauth")
+			return
+		}
+		// プロフィール取得
+		user := twitter.User{}
+		err := twitter.GetUser(c, aToken, &user)
+		if err != nil {
+			c.Redirect(http.StatusSeeOther, "/twitter/oauth")
+			return
+		}
+		// タイムライン取得
+		tl := twitter.UserTimeline{}
+		err = twitter.GetUserTimeline(c, aToken, user.ID, &tl)
+		if err != nil {
+			c.Redirect(http.StatusSeeOther, "/twitter/oauth")
+			return
+		}
+		// ユーザーページ表示
+		c.HTML(http.StatusOK, "twitter.html", gin.H{
+			"title":    "user page",
+			"user":     user,
+			"timeline": tl,
+		})
+	})
+	r.GET("/twitter/oauth", func(c *gin.Context) {
+		loginURL, err := twitter.OAuth(c)
+		if err != nil {
+			c.Redirect(http.StatusSeeOther, "/")
+			return
+		}
+		c.Redirect(http.StatusFound, loginURL)
+	})
+	r.GET("/twitter/callback", func(c *gin.Context) {
+		redirectURL, err := twitter.Callback(c)
+		if err != nil {
+			c.Redirect(http.StatusSeeOther, "/twitter/oauth")
+			return
+		}
+		c.Redirect(http.StatusFound, redirectURL)
+	})
+	r.POST("/twitter/unoauth", func(c *gin.Context) {
+		err := twitter.UnOAuth(c)
+		if err != nil {
+			c.Redirect(http.StatusSeeOther, "/")
+			return
+		}
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "twitter unauthorize successed",
+		})
+	})
+	r.POST("/twitter/post", func(c *gin.Context) {
+		aToken := twitter.GetAccessToken(c)
+		if aToken == nil {
+			c.Redirect(http.StatusSeeOther, "/twitter/oauth")
+			return
+		}
+		post := twitter.NewPost{
+			Status: c.PostForm("content"),
+		}
+		err := twitter.Tweet(c, aToken, &post)
+		if err != nil {
+			c.Redirect(http.StatusSeeOther, "/twitter")
+			return
+		}
+		c.Redirect(http.StatusFound, "/twitter")
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080
